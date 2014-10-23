7b9970f148f8e81b12f3a365fd33c1f039539eec
kazinsser <kazinsser@gmail.com>
2014-10-23 15:26:00 -0400

    sdfasf

--- src/RPGFrame.java
+++ src/RPGFrame.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -9,60 +10,94 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
 public class RPGFrame extends JFrame {
 
-	public RPGFrame(){
+	Color color;
+
+	public RPGFrame() {
 		super("RentPlayGo");
 		setLayout(new BorderLayout());
-		
-		JPanel toolBar = new JPanel();		
+
+		JPanel toolBar = new JPanel();
 		JPanel headerBar = new JPanel();
 		JPanel navigationBar = new JPanel();
 		JPanel blank = new JPanel();
-		
+
 		JButton logoButton = new JButton("Logo");
 		JTextField searchBar = new JTextField("");
-		
-		toolBar.setPreferredSize(new Dimension(1000,80));
-		searchBar.setPreferredSize(new Dimension(200,24));
-				
+
+		toolBar.setPreferredSize(new Dimension(1000, 80));
+		searchBar.setPreferredSize(new Dimension(200, 24));
+
 		JButton homeButton = new JButton("Home");
 		JButton cartButton = new JButton("Cart");
 		JButton myAccountButton = new JButton("My Accout");
 		JButton contactButton = new JButton("Contact");
-		
+
 		toolBar.setLayout(new BorderLayout());
 		headerBar.setLayout(new BorderLayout());
 		navigationBar.setLayout(new FlowLayout());
-		
+
 		headerBar.add(blank, BorderLayout.NORTH);
 		headerBar.add(logoButton, BorderLayout.WEST);
 		headerBar.add(searchBar, BorderLayout.EAST);
-		
+
 		navigationBar.add(homeButton);
 		navigationBar.add(cartButton);
 		navigationBar.add(myAccountButton);
-		navigationBar.add(contactButton);		
-		
+		navigationBar.add(contactButton);
+
 		toolBar.add(headerBar, BorderLayout.CENTER);
 		toolBar.add(navigationBar, BorderLayout.SOUTH);
-		
+
 		add(toolBar, BorderLayout.NORTH);
-		
+
+		Handler handler = new Handler();
+
+		homeButton.addActionListener(handler);
+		cartButton.addActionListener(handler);
+		myAccountButton.addActionListener(handler);
+		contactButton.addActionListener(handler);
+
+	}
+
+	public class Handler implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+
+			String navButtonPressed = e.getActionCommand();
+
+			switch (navButtonPressed) {
+			case "Home":
+				HomePanel homePanel = new HomePanel(color);
+				break;
+			case "Cart":
+				CartPanel cartPanel = new CartPanel();
+				break;
+			case "My Accout":
+				AccountPanel accountPanel = new AccountPanel();
+				break;
+			case "Contact":
+				ContactPanel contact = new ContactPanel();
+				break;
+			}
+		}
 	}
 	
-	public class Handler implements ActionListener{
+	public void addPanels() {
+		removePanels();
+	}
 
-		
-		public void actionPerformed(ActionEvent event) {
-			
-			
-		}
-		
-		
-		
+	public void removePanels() {
+
 	}
 	
 	
 }
+
+
+
+
+
+
+
