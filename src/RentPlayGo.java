33bcc881b8244db1ab3cd4b560727b71ee3b680c
Kevin Zamora <kzamora6900@eagle.fgcu.edu>
2014-10-23 13:17:46 -0400

    Resized

--- src/RentPlayGo.java
+++ src/RentPlayGo.java
@@ -5,7 +5,7 @@ public class RentPlayGo {
 	public static void main(String args[]){
 		RPGFrame mainFrame = new RPGFrame();
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setSize(800,600);
+		mainFrame.setSize(1200,700);
 		mainFrame.setVisible(true);
 	}
 }
